08355c8c18fef48dc7f65dbcf25e0416969ada9c
pjwallace23 <pjwallace23@outlook.com>
2021-10-22 16:13:22 -0400

    sql python code

--- speedtest_script.py
+++ speedtest_script.py
@@ -1,4 +1,6 @@
 import speedtest
+import pyodbc
+
 
 wifi = speedtest.Speedtest()
 ul = wifi.upload()
@@ -7,3 +9,21 @@ print("WiFi DL speed is:")
 print(dl)
 print("WiFi UL speed is:")
 print(ul)
+
+## sql code
+
+#conn = pyodbc.connect('Driver={SQL Server};'
+#                      'Server=server;'
+#                      'Database=database;'
+#                      'Trusted_Connection=yes;')
+
+#cursor = conn.cursor()
+
+#cursor.execute('''
+#               INSERT INTO thing (field1, field2, etc)
+#               VALUES 
+#               (Var1, dl, ul)
+#               ''')
+
+#conn.commit()
+
