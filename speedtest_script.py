f7f0c0212a213aef0a0451435dc04bf42850a2d1
Flymindo <kaahsh386@gmail.com>
2021-11-07 20:54:58 -0500

    Added pushing data to database and run speed tests in parallel

--- speedtest_script.py
+++ speedtest_script.py
@@ -1,57 +1,79 @@
-class Solution(object):
-
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        max_len = 0
-        result = ""
-        if len(s) == 1:
-            return s
-
-        for idx, i in enumerate(s):
-            dif = self.check_odd(s, idx, 1)
-            total_len = 2 * dif + 1
-            if (total_len > max_len):
-                max_len = total_len
-                result = s[idx - dif:idx + dif + 1]
-        for idx, i in enumerate(s):
-            if idx == 0:
-                continue
-            print(idx)
-            dif = self.check_even(s, idx - 1, idx, 0)
-            print(dif)
-            total_len = 2 * dif + 2
-            if (total_len > max_len):
-                max_len = total_len
-                result = s[idx - dif - 1:idx + dif + 1]
-            # if max_len == 0:
-            #     return s[0]
-
-        return result
-
-    def check_odd(self, s, idx, i):
-        if idx - i < 0 or idx + i >= len(s):
-            return 0
-        if s[idx - i] == s[idx + i]:
-            return 1 + self.check_odd(s, idx, i + 1)
-        else:
-            return 0
-
-    def check_even(self, s, left, right, i):
-        print(left)
-        print(right)
-        if left - i < 0 or right + i >= len(s):
-            return i - 1
-        if s[left - i] == s[right + i]:
-            print("in")
-            return self.check_even(s, left, right, i + 1)
-        else:
-            return i - 1
+import pymysql
+import speedtest
+import threading
 
 
-x = Solution()
-print(x.longestPalindrome("abb"))
+
+def test_speed():
+    st = speedtest.Speedtest()
+    dl = st.download()
+    ul = st.upload()
+    
+    mydb = pymysql.connect(
+        host='drone-network-test.cxgmkpckrzov.us-east-2.rds.amazonaws.com',
+        port=3306,
+        user='admin',
+        password='Team16~!',
+        database='network_test_data',
+    )
+
+    cursor = mydb.cursor()
+
+    sql = "INSERT INTO network_result (download, \
+       upload) \
+       VALUES ('%f', '%f')" % \
+       (dl, ul)
+    cursor.execute(sql)
+
+    mydb.commit()
+    mydb.close()
+
+
+
+
+try:
+   t1 = threading.Thread(target = test_speed )
+   t2 = threading.Thread(target = test_speed )
+   t3 = threading.Thread(target = test_speed )
+   t1.start()
+   t2.start()
+   t3.start()
+
+   t1.join()
+   t2.join()
+   t3.join()
+   #data_to_insert = [(download1,upload1),(download2,upload2),
+   #                 (download3,upload3)]
+   
+   #cursor = mydb.cursor()
+   
+   #sql = "INSERT INTO network_result (download, \
+   #upload) \
+   #VALUES ('%f', '%f')"
+   
+   #cursor.executemany(sql,data_to_insert)
+   #mydb.commit
+   #mydb.close()
+   
+except:
+   print("Error: unable to start thread")
+
+
+#mydb = pymysql.connect(
+#    host='drone-network-test.cxgmkpckrzov.us-east-2.rds.amazonaws.com',
+#    port=3306,
+#    user='admin',
+#    password='Team16~!',
+#    database='network_test_data',
+#)
+#data_to_insert = [(download1,upload1),(download2,upload2),
+#                    (download3,upload3)]
+#cursor = mydb.cursor()
+#sql = "INSERT INTO network_result (download, \
+#    upload) \
+#    VALUES ('%f', '%f')"
+#cursor.executemany(sql,data_to_insert)
+#mydb.commit
+#mydb.close()
 
    
