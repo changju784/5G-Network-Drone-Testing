5cada63b9be93b629def600667e3e5715bd5c935
Flymindo <kaahsh386@gmail.com>
2021-11-09 23:37:57 -0500

    Enabled getting distance to the best server and push it to the database

--- speedtest_script.py
+++ speedtest_script.py
@@ -8,6 +8,11 @@ def test_speed():
     st = speedtest.Speedtest()
     dl = st.download()
     ul = st.upload()
+#    server = st.get_closest_servers()
+    server = st.get_best_server()
+    distance_to_server = server['d']
+#    print(distance_to_server)
+#    print(server)
     
     mydb = pymysql.connect(
         host='drone-network-test.cxgmkpckrzov.us-east-2.rds.amazonaws.com',
@@ -20,28 +25,28 @@ def test_speed():
     cursor = mydb.cursor()
 
     sql = "INSERT INTO network_result (download, \
-       upload) \
-       VALUES ('%f', '%f')" % \
-       (dl, ul)
+       upload, distance_to_server) \
+       VALUES ('%f', '%f', '%f')" % \
+       (dl, ul, distance_to_server)
     cursor.execute(sql)
 
     mydb.commit()
     mydb.close()
 
+test_speed()
 
 
-
-try:
-   t1 = threading.Thread(target = test_speed )
-   t2 = threading.Thread(target = test_speed )
-   t3 = threading.Thread(target = test_speed )
-   t1.start()
-   t2.start()
-   t3.start()
-
-   t1.join()
-   t2.join()
-   t3.join()
+#try:
+#   t1 = threading.Thread(target = test_speed )
+#   t2 = threading.Thread(target = test_speed )
+#   t3 = threading.Thread(target = test_speed )
+#   t1.start()
+#   t2.start()
+#   t3.start()
+#
+#   t1.join()
+#   t2.join()
+#   t3.join()
    #data_to_insert = [(download1,upload1),(download2,upload2),
    #                 (download3,upload3)]
    
@@ -55,25 +60,9 @@ try:
    #mydb.commit
    #mydb.close()
    
-except:
-   print("Error: unable to start thread")
+#except:
+#   print("Error: unable to start thread")
 
 
-#mydb = pymysql.connect(
-#    host='drone-network-test.cxgmkpckrzov.us-east-2.rds.amazonaws.com',
-#    port=3306,
-#    user='admin',
-#    password='Team16~!',
-#    database='network_test_data',
-#)
-#data_to_insert = [(download1,upload1),(download2,upload2),
-#                    (download3,upload3)]
-#cursor = mydb.cursor()
-#sql = "INSERT INTO network_result (download, \
-#    upload) \
-#    VALUES ('%f', '%f')"
-#cursor.executemany(sql,data_to_insert)
-#mydb.commit
-#mydb.close()
 
    
