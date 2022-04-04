f26abb5411e822d6701d707e70d4e34d144eba4c
Flymindo <kaahsh386@gmail.com>
2022-04-04 18:53:06 -0400

    Web changes

---
+++ Network_test_web_app/app/src/map.py
@@ -0,0 +1,25 @@
+import gmplot
+from src.apiKey import *
+
+from src.ml_engines.build_dataset import BuildData
+
+
+class Map:
+    @classmethod
+    def __getInstance(cls):
+        return cls.__instance
+
+    @classmethod
+    def instance(cls, *args, **kargs):
+        cls.__instance = cls(*args, **kargs)
+        cls.instance = cls.__getInstance
+        return cls.__instance
+
+    def visualize(self,dataset):
+        total_data = dataset.drop(columns=['id'])
+        lat = total_data['latitude'].values.tolist()
+        lon = total_data['longtitude'].values.tolist()
+        center = [sum(lat) / len(lat), sum(lon) / len(lon)]
+        gmap3 = gmplot.GoogleMapPlotter(center[0], center[1], 15, apikey=google_api)
+        gmap3.scatter(lat, lon, '#000000', size=15, marker=False)
+        gmap3.draw("./app/templates/map_history.html")
